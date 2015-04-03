0f871cf59d1f6224e236ce7c4cf586a832104a36
Gearóid Moroney <hello@gearoid.me>
2015-04-03 16:03:41 +0100

    Big tidy up to make v0.1.0
    Much more usable, with the x object exporting the required functions now.

--- README.md
+++ README.md
@@ -1,30 +1,52 @@
-#x.js v0.0.1
+#x.js v0.1.0
 
 A very simple Javascript library for two-way binding library with no dependencies.
 
-## What it does
-x.js removes the hassle of writing a lot of repetitive code to ensure the data in your Javascript matches what's displayed. It is designed to be as small as possible so does not necessarily cover all scenarios.
+## Demo
+See the `demo.html` file.
+
+## Quick start
+Add the `x` (or `data-x`) attribute to any DOM element. Input fields will update values, and other fields will display these as they change.
+
+
+	<input name="first_name" x="first_name">
+	<select x="gender">
+		<option value="Unspecified">Unspecified</option>
+		<option value="Male">Male</option>
+		<option value="Female">Female</option>
+	</select>
+	<input type="checkbox" x="check">
+
+	Name: <span x="first_name"></span>
+	Gender: <span x="gender"></span>
+	Checkbox: <span x="check"></span>
+
+
+Include the `x.js` file just before the body tag, and initialize it when you want.
+
+	x.initialize()
+
+All these variables can then be accessed and changed in your Javascript on the `x` object. For example, the first name above is `x.first_name`.
 
 ### Uses
 - Bind an input box to a Javascript variable
 - Have text on the page automatically update when data comes in from an XHR
 
-## Demo
-Coming soon
-
 ## Usage
-Include the x.js file just before the closing body tag.
 
-`<script src="x.js">`
+### x.initialize()
 
-Add the data-x attribute on any element you want to bind. The value will be the variable name you can access in your Javascript.
+Call this to initialize x.js. This will look through the DOM for any elements with an x or data-x attribute and bind to them accordingly.
 
-### HTML
+### x.reinitialize()
 
-`<input data-x="username">`
+If you add new elements that need binding, have updated existing bound elements, or need to redo bindings for any reason. This will reset all current bindings, but keep all variables as they are.
 
-`<span data-x="username"></span>`
+### x.getAll()
 
-### Javascript
+This will return an object containing all the currently bound values.
 
-All the values you bind are available on the `xvalues` object. In the example above, the value of the input would be the variable `xvalues.username`.
+## To Do
+
+- Work correctly for radio buttons
+- Prevent cursor from moving to end of a field on input
--- x.js
+++ x.js
@@ -1,39 +1,105 @@
-var nodes = document.querySelectorAll('[data-x]');
-var listeners = {};
-var xvalues = {
-	_attributes: {}
-};
+(function() {
+	var x = {};
+	var events, boundNodes, boundValues;
 
-for(var i=0; i<nodes.length; i++) {
-	(function(){
-		var varName = nodes[i].attributes['data-x'].value;
-		xvalues[varName] = xvalues[varName] || '';
+	// Overrides for fields which do not use the "input" event
+	var EVENT_TYPES = {
+		checkbox: 'change'
+	};
 
-		Object.defineProperty(xvalues, varName, {
-			get: function() {
-				return xvalues._attributes[varName];
-			},
-			set: function(value) {
-				xvalues._attributes[varName] = value;
-				if(listeners[varName]) {
-					for(var i=0; i<listeners[varName].length; i++) {
-						if(typeof listeners[varName][i].value !== 'undefined') {
-							listeners[varName][i].value = value;
-						} else {
-							listeners[varName][i].innerHTML = value;
-						}
-					}
+	x.VERSION = '0.1.0';
+
+	x.initialize = function() {
+		boundNodes = {};
+
+		boundValues = boundValues || { _attributes: {} };
+
+		// Create a DOM element to hang events off
+		events = document.createElement('x');
+
+		bind();
+	};
+
+	x.reinitialize = function() {
+		this.initialize();
+	};
+
+	// @TODO: This should return a copy of the object ideally
+	x.getAll = function() {
+		return boundValues._attributes;
+	};
+
+	var onXSet = function(ev) {
+		if(boundNodes[ev.type]) {
+			for(var i=0; i<boundNodes[ev.type].length; i++) {
+				if(typeof boundNodes[ev.type][i].value !== 'undefined') {
+					boundNodes[ev.type][i].value = x[ev.type];
+				} else {
+					boundNodes[ev.type][i].innerHTML = x[ev.type];
 				}
 			}
-		});
-
-		listeners[varName] = listeners[varName] || [];
-		listeners[varName].push(nodes[i]);
-
-		if(typeof nodes[i].value !== 'undefined') {
-			nodes[i].addEventListener('input', function(ev) {
-				xvalues[varName] = ev.target.value;
-			});
 		}
-	})();
-}
+	};
+
+	var onXInput = function(ev) {
+		var value;
+
+		if(ev.target.type === 'checkbox') {
+			value = ev.target.checked;
+		} else {
+			value = ev.target.value;
+		}
+
+		x[getVarFromNode(ev.target)] = value;
+	};
+
+	var getNodes = function() {
+		return document.querySelectorAll('[x], [data-x]');
+	};
+
+	var getVarFromNode = function(node) {
+		return (node.attributes.x || node.attributes['data-x']).value;
+	};
+
+	var bind = function() {
+		var nodes = Array.prototype.slice.call(getNodes());
+
+		nodes.forEach(function(node) {
+			var varName = getVarFromNode(node);
+
+			// Not using shorthand here because it will invoke the get on subsequent runs
+			if(!boundValues[varName]) {
+				boundValues[varName] = '';
+			}
+
+			if(!x.hasOwnProperty(varName)) {
+
+				// @TODO: These get and set functions need to be better - there shouldn't be
+				// a separate instance for each varName
+				Object.defineProperty(x, varName, {
+					get: function() {
+						return boundValues._attributes[varName];
+					},
+					set: function(value) {
+						// Only do something if the value has changed
+						if(boundValues._attributes[varName] !== value) {
+							boundValues._attributes[varName] = value;
+							events.dispatchEvent(new Event(varName));
+						}
+					}
+				});
+			}
+
+			events.addEventListener(varName, onXSet);
+
+			boundNodes[varName] = boundNodes[varName] || [];
+			boundNodes[varName].push(node);
+
+			if(typeof node.value !== 'undefined') {
+				node.addEventListener(EVENT_TYPES[node.type] || 'input', onXInput);
+			}
+		});
+	};
+
+	this.x = x;
+}).call(this);
